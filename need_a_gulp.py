294cc6938b630c023957c65c4cdfd752798525da
thebasilugo <115357283+thebasilugo@users.noreply.github.com>
2022-10-08 18:56:24 +0100

    Update need_a_gulp.py
    Further comments for understanding included.
    Corrections on grammar and vocabulary have been made.

--- need_a_gulp.py
+++ need_a_gulp.py
@@ -1,11 +1,11 @@
-#
 # Welcome to Need a gulp!
 ####################################################################
-
+    # Request the name of the user.
 user_name = input("Welcome to Need a gulp, I'm Slushious. What is your name? \n").lower()
 if user_name != 'thebasilugo':
     print("Oh, I thought you were my programmer, Ugochukwu. Anyways, Welcome to Need a gulp, " + user_name + "!")
 
+   # Request the categorization of user.
     print("So, " + user_name + ", are you a staff or a customer?")
     user_input_any = input("Please use '1' for staff and '2' for customer. \n")
     if user_input_any == '1':
@@ -18,9 +18,9 @@ if user_name != 'thebasilugo':
 
     else:
         print("Sorry, " + user_name + ". Your input is invalid. Please Try again.")
-    # Should be able to refer back to beginning so code can be efficiently run over time.
-    # Should maybe have a "End" to end process, instead of forcing customers to buy.
+    # Should be able to refer to beginning of code (using a function) so code can be efficiently run over time.
+    # Should maybe have a "End" to end the process, instead of forcing customers to buy refreshments.
 
 elif user_name == 'thebasilugo':
-    print("Welcome chief, I hope you're having a lovely day.")
-    #should automatically log 'thebasilugo' in as a staff (or eexecutive, if dashboard should exist)
+    print("Welcome Chief, I hope you're having a lovely day.")
+    #should automatically log 'thebasilugo' in as a staff (or executive, if executive_dashboard should exist)
